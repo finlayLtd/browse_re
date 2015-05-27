5275d7451c9a3080a9589dcfb7a4f2fc4d1ed10c
iTyphoons <hon.yang88@hotmail.com>
2015-05-26 21:09:49 -0400

    Update fixtures, simplify code

--- streams/github.js
+++ streams/github.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var through2 = require('through2');
+var through = require('through');
 var exec = require('child_process').exec;
 var path = require('path');
 var fs = require('fs');
@@ -47,14 +47,16 @@ function makeGetBase() {
  */
 module.exports = function () {
   var getBase = makeGetBase();
-  return through2.obj(function (comment, enc, callback) {
+  return through(function (comment) {
+    this.pause();
     getBase(comment.context.file, function (base, root) {
       comment.context.path = comment.context.file.replace(root + '/', '');
       comment.context.github = base +
         comment.context.file.replace(root + '/', '') +
         '#L' + comment.context.loc.start.line + '-' +
         'L' + comment.context.loc.end.line;
-      callback(null, comment);
-    });
+      this.push(comment);
+      this.resume();
+    }.bind(this));
   });
 };
